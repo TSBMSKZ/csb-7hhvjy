39a616fef579235483399d17fb534c5ab330a0da
Code Sandbox <hello@codesandbox.io>
2023-10-17 12:18:49 +0000

    Deployment from Codesandbox.

---
+++ index.e8d2b542.js
@@ -0,0 +1,4 @@
+const e=()=>{document.getElementById("addText").value,document.getElementById("addText").value="";//divタグ生成
+let e=document.createElement("div");console.log(e)};document.getElementById("addButton").addEventListener("click",()=>e());//# sourceMappingURL=index.e8d2b542.js.map
+
+//# sourceMappingURL=index.e8d2b542.js.map
